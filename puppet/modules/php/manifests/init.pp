ab546a198b1189898d2e9e98f263dc744af47d09
Daniel Platt <dan@ukwebmedia.com>
2014-09-24 16:29:59 +0100

    additional php modules

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,7 +1,6 @@
 class php {
 
-	package { ['php5-fpm',
-						 'php5-cli']:
+	package { ['php5-fpm', 'php5-cli', 'php5-xdebug', 'php5-mysql', 'php5-intl', 'php5-apcu']:
 	  ensure => present,
 	  require => Exec['apt-get update'],
 	}
